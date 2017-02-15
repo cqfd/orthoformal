0330dc4e627e377762999e0ccb89929085692186
Alan O'Donnell <alan.m.odonnell@gmail.com>
2017-02-15 11:51:56 -0500

    Unused imports.

--- examples/index.js
+++ examples/index.js
@@ -1,12 +1,10 @@
 // @flow
 
-import React from 'react'
 import ReactDOM from 'react-dom'
 
 import TextInput from './text_input'
 import Checkbox from './checkbox'
 
-import * as orthoformal from '../index'
 import { type Form, array, object, map, filter } from '../index'
 
 const number = map(TextInput.form('age: '), parseInt)
